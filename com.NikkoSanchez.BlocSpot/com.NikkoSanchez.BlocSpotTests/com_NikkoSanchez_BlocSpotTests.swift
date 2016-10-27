a82e094ca42a199d80a22b45da86bde5997d5132
Nikko Sanchez <neeks08@gmail.com>
2016-10-27 16:41:39 -0700

    Basic Template

---
+++ com.NikkoSanchez.BlocSpot/com.NikkoSanchez.BlocSpotTests/com_NikkoSanchez_BlocSpotTests.swift
@@ -0,0 +1,36 @@
+//
+//  com_NikkoSanchez_BlocSpotTests.swift
+//  com.NikkoSanchez.BlocSpotTests
+//
+//  Created by Nikko on 10/27/16.
+//  Copyright © 2016 Nikko. All rights reserved.
+//
+
+import XCTest
+@testable import com_NikkoSanchez_BlocSpot
+
+class com_NikkoSanchez_BlocSpotTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
